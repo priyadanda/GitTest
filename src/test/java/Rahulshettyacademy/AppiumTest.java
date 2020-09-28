8eb9c53dbefc307b66c8b6eb176c8139b59976bb
priya <krishnapriya.danda@gmail.com>
2020-09-28 22:06:30 +1000

    first commit

---
+++ src/test/java/Rahulshettyacademy/AppiumTest.java
@@ -0,0 +1,15 @@
+package Rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
