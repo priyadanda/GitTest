1eb0e43ecdde836ab3b6a148f627d94e9d879bf4
priya <krishnapriya.danda@gmail.com>
2020-09-29 19:14:04 +1000

    Australia Timezone guy

--- src/test/java/Rahulshettyacademy/RESTAPITest.java
+++ src/test/java/Rahulshettyacademy/RESTAPITest.java
@@ -7,6 +7,9 @@ public class RESTAPITest {
 	@Test
 	public void PostJira() {
 		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
+		
 	}
 	@Test
 	public void DeleteTwitter() {
