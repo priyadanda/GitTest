b035768567c11cfd0c345a9544f6aed8b4957c35
priya <krishnapriya.danda@gmail.com>
2020-09-29 22:42:51 +1000

    Australia guy new class file

--- src/test/java/Rahulshettyacademy/Mobiletest.java
+++ src/test/java/Rahulshettyacademy/Mobiletest.java
@@ -1,5 +1,12 @@
 package Rahulshettyacademy;
 
+import org.testng.annotations.Test;
+
 public class Mobiletest {
+	
+	@Test
+	public void MobileAutomation() {
+		System.out.println("MobileAutomation");
+	}
 
 }
